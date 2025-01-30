bb012a1b4d4cb8f1b0a81e75731c872d7c9f2e36
200~mahmoud-mousa2468 <mo.mousa2468@gmail.com>
2025-01-30 11:20:23 +0200

    What is Collections

--- section 2/Program.cs
+++ section 2/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Collection : Built-in DS in C#
+            // Array: Container
+
+            // Collections in C#
+            // Lists
+            // HashTables
+
+            // Generic - non-Generic
+            // lists:
+            // Non-Generic Lists: ArrayList -Stack-Queue
+            // Generic Lists : List,Stack,Queue,LinkedList
+
+            // HashTables:
+            // Non-Generics HashTables : HashTable
+            // Generics HashTable : Dictionary -SortedDictionary
         }
     }
 }
