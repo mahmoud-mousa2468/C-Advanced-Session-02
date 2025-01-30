5492eca93dedbc2d793786378a870ab29d15276c
200~mahmoud-mousa2468 <mo.mousa2468@gmail.com>
2025-01-30 14:34:03 +0200

    Non-Generic Arraylist

---
+++ section 2/Employee.cs
@@ -0,0 +1,23 @@
+﻿namespace section_2
+{
+    internal class Employee : IComparable
+    {
+        public int Id { get; set; }
+        public int Age { get; set; }
+        public string Name { get; set; }
+
+        public int CompareTo(object? obj)
+        {
+            if (obj is Employee E)
+            {
+                return this.Age.CompareTo(E.Age);
+            }
+            return -1;
+        }
+
+        public override string ToString()
+        {
+            return $"Id {Id} : Name: {Name} : Age : {Age}";
+        }
+    }
+}
--- section 2/Program.cs
+++ section 2/Program.cs
@@ -1,9 +1,12 @@
-﻿namespace section_2
+﻿using System.Collections;
+
+namespace section_2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            #region What is Collections
             // Collection : Built-in DS in C#
             // Array: Container
 
@@ -18,7 +21,58 @@
 
             // HashTables:
             // Non-Generics HashTables : HashTable
-            // Generics HashTable : Dictionary -SortedDictionary
+            // Generics HashTable : Dictionary -SortedDictionary 
+            #endregion
+
+            #region Non-Generic Arraylist
+            // lists 
+            // Non-Generic list : Arraylist
+            // ArrayList Like Array
+            // Array Based
+            // Index
+            // Array Indexed Collection : Fixed Size
+            // ArrayList Indexed Collection : Dynamic Size
+            // it's default capacity is 4 and any expand will multible it by 2
+            // Index: 0 1 2 3 4 5
+            // items: 4 5 6 7 8 1
+            // Size : 6
+
+            // Arr[1]----> 5
+            //int[] Numbers = { 9, 8, 7, 6, 1, 2, 3, 4, 5 };
+            //Console.WriteLine(Numbers[1]);
+            //Numbers[1] = 12;
+            //Console.WriteLine(Numbers[1]);
+
+            //ArrayList list = new ArrayList();
+            //ArrayList list = new ArrayList(new int[] { 9, 8, 7, 6, 1, 2, 3, 4, 5 });
+            ////ArrayList list = new ArrayList(5);
+            //Console.WriteLine($"Count : {list.Count} :: Capacity: {list.Capacity}");
+            //list.Add(10);
+            //int index = list.BinarySearch(9);
+
+            //Console.WriteLine($"Count : {list.Count} :: Capacity: {list.Capacity}");
+            //Console.WriteLine(list.IsFixedSize); // return false
+            //Console.WriteLine(list.IsReadOnly);  // return false
+
+            //list.Clear();// clear the list
+            //bool Flag = list.Contains(20);
+            //Console.WriteLine(Flag);
+
+
+            //int[] Arr = new int[4];
+
+            //list.CopyTo(Arr);
+            //list.CopyTo(2,Arr,1,1);
+
+            //list.GetRange(1, 2);
+
+            //int i = list.IndexOf(10);
+            //Console.WriteLine(i);
+            //list.Remove(5);
+
+            //list.Sort();// sort the list
+
+            #endregion
         }
     }
 }
